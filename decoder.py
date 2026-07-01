63872510a35dc9fb74825fb75306eec2a2a912ed
boinkee <testere.boinkee@gmail.com>
2026-07-01 16:47:29 +0300

    Update decoder.py uppercasse lowercasse

--- decoder.py
+++ decoder.py
@@ -1,6 +1,6 @@
 '''Module for the whole program to work (Base64 module)'''
 import base64
-linecount=0
+LINECOUNT=0
 infile = input("Enter input file\n")
 with open(infile, "r", encoding="utf-8") as fyle, \
      open(f"{infile}.decoded", "w", encoding="utf-8") as out:
@@ -10,7 +10,7 @@ with open(infile, "r", encoding="utf-8") as fyle, \
         if not encoded:
             continue
         linecount +=1
-        print("line", linecount)
+        print("line", Linecount)
         decoded = base64.b64decode(encoded).decode("utf-8", errors="replace").rstrip("\r\n")
         out.write(decoded + "\n")
 print("wrote", linecount, "lines into", f"{infile}.decoded")
